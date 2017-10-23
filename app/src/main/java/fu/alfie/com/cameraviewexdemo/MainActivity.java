8f8a87a48b2d1c4c4fccb5240482a82561ec3830
傅俊皓 <alfie.fu@toplogis.com>
2017-10-23 15:09:31 +0800

    可正常拍照並旋轉壓縮為小圖

--- app/src/main/java/fu/alfie/com/cameraviewexdemo/MainActivity.java
+++ app/src/main/java/fu/alfie/com/cameraviewexdemo/MainActivity.java
@@ -1,24 +1,45 @@
 package fu.alfie.com.cameraviewexdemo;
 
+import android.content.pm.ActivityInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
 import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private FrameLayout preview;
     private Camera mCamera;
     private CameraView cameraView;
+    private ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
         mCamera = getCameraInstance();
+        mCamera.setDisplayOrientation(90);
         preview = (FrameLayout) findViewById(R.id.preview1);
+        imageView = (ImageView) findViewById(R.id.imageView1);
         cameraView = new CameraView(this, mCamera);
         preview.addView(cameraView);
+
+        Button captureButton = (Button)findViewById(R.id.button);
+        captureButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mCamera.takePicture(null, null, pictureCallback);
+            }
+        });
 
         mCamera.startPreview();
     }
@@ -33,4 +54,28 @@ public class MainActivity extends AppCompatActivity {
         }
         return c; // returns null if camera is unavailable
     }
+
+    Camera.PictureCallback pictureCallback = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            Bitmap bitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+            if(bitmap==null){
+                Toast.makeText(MainActivity.this, "Captured image is empty", Toast.LENGTH_LONG).show();
+                return;
+            }
+            Matrix mtx = new Matrix();
+            mtx.postRotate(90);
+            imageView.setImageBitmap(scaleDownBitmapImage(bitmap, 300, 200 ));
+        }
+    };
+
+    private Bitmap scaleDownBitmapImage(Bitmap bitmap, int newWidth, int newHeight){
+        Bitmap resizedBitmap = Bitmap.createScaledBitmap(bitmap, newWidth, newHeight, true);
+        Matrix mtx = new Matrix();
+        mtx.postRotate(90);
+        Bitmap rotatedBitmap = Bitmap.createBitmap(resizedBitmap, 0, 0, newWidth/2, newHeight, mtx, true);
+        return rotatedBitmap;
+    }
+
+
 }
